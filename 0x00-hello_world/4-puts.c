b10f15f68964454fd9ff1dfb0a74869bdc487021
root <nazycole@gmail.com>
2023-01-03 04:44:41 -0800

    0x00-hello_world: 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a sentence for puts.c
+ * Return:0 if exited properly, otherwise a non-zero value
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
