f9a6353c12f271bcf78c4f2a3a106e5c2c666da7
root <nazycole@gmail.com>
2023-01-03 11:45:51 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Alphabet game
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar("\n");
+		return (0);
+}
