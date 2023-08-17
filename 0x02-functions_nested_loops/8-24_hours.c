ece1a9d9d04a37492c79373366d669c497b783b7
root <nazycolegmail.com>
2023-08-16 23:15:02 -0700

    charcter lentgth

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day for 24, starting from 00:00 to 23:59.
+ * jack_bauer - function that prints every minute of the day for 24
  * h = hour, m = minutes
  * / 10 allows second digit to rotate
  * for loop breaks before passing 24:00
