17bf407b4c0e4b916a35e324bbadfac13e5c32c4
Nazzoe”git config --global user.email nazycole@gmail.comgit config --global user.name Nazzoe” <nazycole@gmail.com>
2023-11-02 16:42:22 -0700

    Static lib libmy

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for capital letters
+ * @c: the letter to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit range
+ * @c: digit integer
+ * Return: 1 if true, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
